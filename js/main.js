33853021391354bbcad94d04d59feaf4907fa791
Denis Shved <limonye@mail.ru>
2021-03-12 18:36:07 +0200

    create: adaptive for hotel, add yandex map api

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ const swiper = new Swiper('.swiper-container', {
   },
 
 });
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [7.838166, 98.298806],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [7.838166, 98.298806]
+            },
+            properties: {
+                iconContent: 'Grand Hilton Hotel',
+                hintContent: 'You are welcome'
+            }
+        }, {
+            preset: 'islands#blackStretchyIcon',
+            draggable: true
+        })
+
+    myMap.geoObjects
+        .add(myGeoObject)
+}
