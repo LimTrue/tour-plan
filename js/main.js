b40dc825644f04b00a54001f9be3a0e2476a62f7
Denis Shved <limonye@mail.ru>
2021-03-15 12:38:09 +0200

    create: reviews section and fixed hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-
+const hotelSlider = new Swiper('.hotel-slider', {
+    loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+   loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
